11874947782ec9b2119c7295edacde9d1d12b830
Bibikau Maxim <bib.maxim@gmail.com>
2025-08-22 22:21:21 +0300

    Added redis env password

--- src/utils/redis_client.py
+++ src/utils/redis_client.py
@@ -15,6 +15,7 @@ async def init_redis():
         redis_client = Redis(
             host=os.getenv('REDIS_HOST', 'localhost'),
             port=int(os.getenv('REDIS_PORT', 6379)),
+            password=os.getenv('REDIS_PASSWORD', None),
             decode_responses=True
         )
         pong = await redis_client.ping()
