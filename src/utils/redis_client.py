17f339397a406172650c2b4b503f764c16a7374f
Bibikau Maxim <bib.maxim@gmail.com>
2025-09-01 19:38:05 +0300

    Fixed redis storage issue being created with localhost by default

--- src/utils/redis_client.py
+++ src/utils/redis_client.py
@@ -1,11 +1,9 @@
 import os
-from dotenv import load_dotenv
 from redis.asyncio import Redis
 
 from src.utils.logger import get_logger
 
 
-load_dotenv()
 logger = get_logger()
 redis_client = None
 
@@ -13,9 +11,9 @@ async def init_redis():
     try:
         global redis_client
         redis_client = Redis(
-            host=os.getenv('REDIS_HOST', 'localhost'),
-            port=int(os.getenv('REDIS_PORT', 6379)),
-            password=os.getenv('REDIS_PASSWORD', None),
+            host=os.environ.get('REDIS_HOST', 'localhost'),
+            port=int(os.environ.get('REDIS_PORT', 6379)),
+            password=os.environ.get('REDIS_PASSWORD', None),
             decode_responses=True
         )
         pong = await redis_client.ping()
